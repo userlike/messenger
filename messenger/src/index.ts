59846fbecdcabacc1ce84eec8a98a0dd7d35a8b9
Anıl Anar <anilanar@users.noreply.github.com>
2020-07-30 17:11:55 +0200

    refactor: get rid of app_key (#1)
    affects: @userlike/messenger, @userlike/messenger-internal

--- messenger/src/index.ts
+++ messenger/src/index.ts
@@ -8,14 +8,13 @@ import {
 export const createMessenger: CreateMessenger = async (
   opts
 ): Promise<ActionResult<string, AllApis>> => {
-  const { createMessenger, app_key, widget_key, config } = await loadWidget(
+  const { createMessenger, widget_key, config } = await loadWidget(
     window,
     opts.widgetKey,
     opts.baseUrl
   );
 
   return createMessenger(opts.version)({
-    app_key,
     widget_key,
     config,
   });
