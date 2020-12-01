c95a432a83af18b531bb8c10854662e151394804
Anil Anar <anilanar@hotmail.com>
2020-12-01 11:04:13 +0100

    refactor: bring back (now legacy) pure loader utility fns
    affects: @userlike/messenger-internal

--- packages/messenger-internal/src/index.ts
+++ packages/messenger-internal/src/index.ts
@@ -3,6 +3,7 @@ export * from "./v1/Observable";
 export * from "./types";
 export * from "./versioning";
 export * from "./loader";
+export * from "./legacy-loader";
 
 export * as v0 from "./v0";
 export * as v1 from "./v1";
