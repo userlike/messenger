9c9cfbd3f1fe0009c75d36cfe77e66dc1c422e9a
Anil Anar <anilanar@hotmail.com>
2020-12-01 10:38:25 +0100

    refactor: impl asset loading from umm manifest
    affects: @userlike/messenger-internal
    
    BREAKING CHANGE:
    The way UMM scripts are loaded has significantly changed.

---
+++ packages/messenger-internal/src/loader/create-frame.ts
@@ -0,0 +1,24 @@
+import { uniqueId } from "./unique-id";
+
+export const createIFrame = async (): Promise<HTMLIFrameElement> => {
+  const iframe = document.createElement("iframe");
+  iframe.id = `userlike-frame-${uniqueId()}`;
+  iframe.setAttribute(
+    "style",
+    `
+    position: absolute !important;
+    opacity: 0 !important;
+    height : 1px !important;
+    top : 0 !important;
+    left : 0 !important;
+    border : none !important;
+    display : block !important;
+    z-index : -1 !important;
+  `
+  );
+
+  return new Promise<HTMLIFrameElement>((resolve) => {
+    iframe.addEventListener("load", () => resolve(iframe));
+    document.body.appendChild(iframe);
+  });
+};
